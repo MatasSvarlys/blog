142bf4ea9d4db8c05f8c0a98d4040e9bd47f197b
MatasSvarlys <matas.svarlys3@gmail.com>
2024-06-10 09:21:21 +0300

    added message indexing to prepare for draggable notes

--- src/components/DraggableNoteList.js
+++ src/components/DraggableNoteList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-export default function DraggableNoteList({ notes }){
+export default function DraggableNoteList({ notes, debugMode }){
 
-    
+    notes = notes.sort((a, b) => a.displayIndex - b.displayIndex);
     return (
         <ul>
             {notes.map((note, index) =>{
                 return (
-                <li key={index}>
-                    {note.noteText}, {note.timeToFinish} min, index: {note.displayIndex}
-                </li>
+                    <li key={index}>
+                        {note.noteText}, {note.timeToFinish} min
+                        {debugMode?(', index: '+ note.displayIndex):''}
+                    </li>
                 );})}
         </ul>
     );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,8 @@ import DraggableNoteList from './DraggableNoteList';
 
 export default function Todo(){
     const [notes, setNotes] = useState([]);
-    
+    const [debugMode, setDebugMode] = useState(true);
+
     useEffect(() => {
         fetchNotes();
     }, []);
@@ -44,22 +45,21 @@ export default function Todo(){
     const groupedNotesByDate = groupNotes(notes);
     return(
         <>
-            <NoteInsertPanel fetchNotes={fetchNotes} orderedNotes={groupedNotesByDate}/>
+          <NoteInsertPanel fetchNotes={fetchNotes} orderedNotes={groupedNotesByDate}/>
+          {debugMode?
             <button onClick={handleDeleteLastNote}>Delete last note</button>
-            <div>Todo list:</div>
-            {notes && Object.keys(groupedNotesByDate).length > 0 ? (
-                <ul>
-                    {Object.keys(groupedNotesByDate).map((finishDate) => (
+            : null
+          }
+          {notes && Object.keys(groupedNotesByDate).length > 0 ? (
+              <ul>
+                  {Object.keys(groupedNotesByDate).map((finishDate) => (
                     <li key={finishDate}>
                         <h3>Date: {finishDate?finishDate:"not specified"}</h3>
-                        <DraggableNoteList notes={groupedNotesByDate[finishDate]}/>
+                        <DraggableNoteList notes={groupedNotesByDate[finishDate]} debugMode={debugMode}/>
                     </li>
-                    ))}
-                </ul>
-                ) : (
-                <li>No notes found.</li>
-            )}
-
+                  ))}
+              </ul>
+              ) : (<li>No notes found.</li>)}
         </>
     );
 }
